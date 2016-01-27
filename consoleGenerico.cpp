4914279ec9ff5e9fce199e3c244c67f7c4adacc5
Marcio Reis <mahrcio@msn.com>
2016-01-27 16:41:40 -0300

    cpp

---
+++ consoleGenerico.cpp
@@ -0,0 +1,19 @@
+#include <consoleGenerico.h>
+#include <string.h>
+using namespace std;
+
+void ConsoleGenerico::iniciarJogo(){
+	
+}
+	
+void ConsoleGenerico::finalizarJogo(){
+	
+}
+	
+string ConsoleGenerico::getVersaoFirmware(){
+	
+}
+	
+string ConsoleGenerico::getMarca(){
+	
+}
